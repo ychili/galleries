cc84db038735f3aeddc9f47282db5a9715f65683
Ethan Montgomery <ethan.montgomery.30@gmail.com>
2024-10-07 18:48:15 -0400

    Remove relatedtag from doctested modules
    Reason: there aren't any doctests to run.

--- tests/test_doctest.py
+++ tests/test_doctest.py
@@ -3,12 +3,12 @@
 import doctest
 import unittest
 
-from galleries import galleryms, refresh, relatedtag, util
+from galleries import galleryms, refresh, util
 
 
 def load_tests(unused_loader, tests, unused_ignore):
     tests.addTests(doctest.DocFileSuite("doctest.rst"))
-    for module in (galleryms, refresh, relatedtag, util):
+    for module in (galleryms, refresh, util):
         tests.addTests(doctest.DocTestSuite(module))
     return tests
 
