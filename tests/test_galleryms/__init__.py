2abee2c94b38d07793aeba3623ef33b3994748f7
Ethan Montgomery <ethan.montgomery.30@gmail.com>
2024-12-03 11:51:18 -0500

    Use more stable way of comparing OverlapTables

--- tests/test_galleryms/__init__.py
+++ tests/test_galleryms/__init__.py
@@ -3,9 +3,7 @@ TAG_SETS = [set("ABC"), set("BCD"), set("AB"), set("DEF"), set("LMNOP")]
 
 class TestOverlapTable:
     def _tables_equal(self, table0, table1):
-        """Equality test for two ``OverlapTable``s
-
-        Compares on private attribute _table, so don't rely on this.
-        """
-        # pylint: disable=protected-access
-        return table0.n_sets == table1.n_sets and table0._table == table1._table
+        """Equality test for two ``OverlapTable``s"""
+        return table0.n_sets == table1.n_sets and (
+            list(table0.pairs_overlaps()) == list(table1.pairs_overlaps())
+        )
