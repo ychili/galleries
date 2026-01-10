40019770861b18cbdd978658cbc717a4e03a524a
Ethan Montgomery <ethan.montgomery.30@gmail.com>
2026-01-10 14:02:28 -0500

    Add unit test for tagcount.count

--- tests/test_tagcount.py
+++ tests/test_tagcount.py
@@ -11,6 +11,11 @@ tag_sets_strategy = hypothesis.strategies.iterables(
 )
 
 
+@hypothesis.given(tag_sets_strategy, hypothesis.strategies.booleans())
+def test_fuzz_count(tag_sets, reverse):
+    assert galleries.tagcount.count(tag_sets, reverse=reverse) == 0
+
+
 @hypothesis.given(tag_sets_strategy)
 def test_fuzz_summarize(tag_sets):
     assert galleries.tagcount.summarize(tag_sets) == 0
