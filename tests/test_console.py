481f7c1ede28cde19d492bb48b53bef8747eba3a
Ethan Montgomery <ethan.montgomery.30@gmail.com>
2026-08-22 11:44:33 -0400

    Convert test_console.py to pytest style
    After some regular `assert`s made their way into the file in
    99894e6d4e0d0089ef94c1640c82ca1af774892a, I converted the whole file from
    unittest style to pytest style with the help of unittest2pytest.

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,14 +1,17 @@
-"""Unit tests for console"""
-
-import unittest
+"""Unit tests for console, using pytest"""
 
 import hypothesis
 import hypothesis.strategies as st
+import pytest
 
 import galleries.console
 
 
-class TestFieldFormat(unittest.TestCase):
+class ConsoleTestCase:
+    pass
+
+
+class TestFieldFormat(ConsoleTestCase):
     @hypothesis.given(
         st.text().filter(
             lambda s: s not in galleries.console.FieldFormat.COLORS
@@ -17,14 +20,12 @@ class TestFieldFormat(unittest.TestCase):
     )
     def test_unknown_arguments(self, key):
         # Unknown color arguments raise KeyError:
-        with self.assertRaises(KeyError):
+        with pytest.raises(KeyError):
             galleries.console.FieldFormat.from_names(-80, fg=key)
-        with self.assertRaises(KeyError):
+        with pytest.raises(KeyError):
             galleries.console.FieldFormat.from_names(-80, bg=key)
         # Unknown effect arguments are accepted:
-        self.assertIn(
-            key, galleries.console.FieldFormat.from_names(-80, effect=key).sgr
-        )
+        assert key in galleries.console.FieldFormat.from_names(-80, effect=key).sgr
 
     lines_strategy = st.iterables(st.text())
 
@@ -33,7 +34,7 @@ class TestFieldFormat(unittest.TestCase):
         null = galleries.console.FieldFormat(-80)
         lines_in = list(lines)
         lines_out = list(null.colorize(lines_in))
-        self.assertEqual(lines_in, lines_out)
+        assert lines_in == lines_out
 
     @hypothesis.given(lines_strategy)
     def test_colorize_normal(self, lines):
@@ -41,10 +42,10 @@ class TestFieldFormat(unittest.TestCase):
         lines_in = list(lines)
         for line_in, line_out in zip(lines_in, blue.colorize(lines_in), strict=True):
             expected_out = f"\033[34m{line_in}\033[0m"
-            self.assertEqual(line_out, expected_out)
+            assert line_out == expected_out
 
 
-class TestTabulator(unittest.TestCase):
+class TestTabulator(ConsoleTestCase):
     def test_integer_indices(self):
         ff = {0: galleries.console.FieldFormat(10)}
         tabulator = galleries.console.Tabulator(ff)
@@ -55,23 +56,23 @@ class TestTabulator(unittest.TestCase):
                 ["Excepteur sint occaecat cupidatat non proident"],
             ]
         )
-        self.assertEqual(len(list(tabulator.tabulate(rows))), 19)
+        assert len(list(tabulator.tabulate(rows))) == 19
 
     def test_format_rectification(self):
         tabulator = galleries.console.Tabulator({0: 10})
-        self.assertEqual(tabulator.field_fmts, {0: galleries.console.FieldFormat(10)})
+        assert tabulator.field_fmts == {0: galleries.console.FieldFormat(10)}
 
     def test_tabulate_empty_input(self):
         tabulator = galleries.console.Tabulator({0: 10})
-        self.assertFalse(list(tabulator.tabulate(iter([]))))
+        assert not list(tabulator.tabulate(iter([])))
 
     def test_tabulate_empty_values(self):
         tabulator = galleries.console.Tabulator({0: 10})
         rows = iter([[""], [""], ["12345"], [""]])
-        self.assertEqual(len(list(tabulator.tabulate(rows))), 4)
+        assert len(list(tabulator.tabulate(rows))) == 4
 
 
-class TestDistribute(unittest.TestCase):
+class TestDistribute(ConsoleTestCase):
     COLUMN_MAX = 0x4000
 
     @hypothesis.given(n=st.integers(), k=st.integers(min_value=1, max_value=COLUMN_MAX))
@@ -79,7 +80,3 @@ class TestDistribute(unittest.TestCase):
         result = list(galleries.console.distribute(n, k))
         assert sum(result) == n
         assert len(result) == k
-
-
-if __name__ == "__main__":
-    unittest.main()
